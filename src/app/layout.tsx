6380981af0b5b9c5c9f820d9050d09d367cc11a3
Matthew Zienert <mzienert@gmail.com>
2025-02-11 22:51:19 -0700

    cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { ThemeProvider } from "@/components/theme-provider"
 import { Providers } from './providers'
 
 const geistSans = Geist({
