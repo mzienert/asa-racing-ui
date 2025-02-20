acbb61cdc6f638732bb06a0b728d8acdc87cdc5c
Matthew Zienert <mzienert@gmail.com>
2025-02-20 14:15:30 -0700

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_COGNITO_USER_POOL_ID: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || 'placeholder',
+    NEXT_PUBLIC_COGNITO_CLIENT_ID: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID || 'placeholder'
+  }
 };
 
 export default nextConfig;
